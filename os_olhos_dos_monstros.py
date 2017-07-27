883b10ebacf4e3f9bb24783619af0ed9e2b7e162
christian <chrishg@gmail.com>
2017-07-27 18:58:27 -0300

    adicionado comentário inicial, removida função cls().

--- os_olhos_dos_monstros.py
+++ os_olhos_dos_monstros.py
@@ -1,19 +1,21 @@
 """
 Os Olhos dos Monstros
 versão em Python por Christian Haagensen Gontijo, 30/06/2017
-"""
 
+Você está encurralado! Para onde quer que se volte, os olhos frios e assustadores de um monstro
+do espaço o espreitam por um momento, antes que o animal se esconda atrás de uma pedra. Pouco a
+pouco, os monstros se aproximam de você, tentando envolvê-lo em seus tentáculos.
+
+Felizmente, você está com a sua pistola de prótons. Os olhos dos monstros aparecem em quatro
+lugares diferentes da tela, que correspondem às teclas de 1 a 4. Se você apertar a tecla certa
+enquanto os olhos do monstro estiverem na tela, ele será destruído. Existem 10 monstros... à
+medida que você os destrói, sua chance de escapar aumenta.
+"""
 import time
 import random
 from colorconsole import terminal  # obrigado, Nilo! <https://github.com/lskbr/colorconsole>
 
 screen = terminal.get_terminal(conEmu=False)
-
-
-def cls():
-    screen.clear()
-    screen.gotoXY(0, 0)
-
 
 print("Os Olhos dos Monstros")
 
@@ -37,7 +39,7 @@ for tentativas in range(11):
     else:
         linha = 10
         coluna = 7
-    cls()
+    screen.clear()
     screen.gotoXY(coluna, linha)
     print("OO")  # é, ISSO são os olhos do monstro... :-p
 
@@ -53,7 +55,7 @@ for tentativas in range(11):
 
     if tentativa == resultado:
         acertos += 1
-        cls()
+        screen.clear()
         screen.gotoXY(coluna, linha)
         print("*")
         time.sleep(5)
